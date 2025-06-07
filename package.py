27f96daa53c83976e21c927b80003e46af283f60
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2025-06-07 19:35:46 -0400

    Remove cgcmake submodule, change cmakes

---
+++ package.py
@@ -0,0 +1,33 @@
+name = 'cvwrap'
+
+version = '1.0.0.hh.1.0.0'
+
+authors = [
+    'Chad V.',
+]
+
+description = '''Maya plugin for rigging'''
+
+with scope('config') as c:
+    import os
+    c.release_packages_path = os.environ['HH_REZ_REPO_RELEASE_EXT']
+    c.build_thread_count = 1
+    c.plugins.release_hook.hh_emailer.recipients = []
+
+requires = [
+    "maya",
+]
+
+private_build_requires = [
+]
+
+variants = [
+]
+
+def commands():
+    env.REZ_CVWRAP_ROOT = '{root}'
+    env.LD_LIBRARY_PATH.prepend('{root}/plug-ins')
+    env.PYTHONPATH.prepend('{root}/scripts')
+
+
+uuid = 'repository.cvwrap'
